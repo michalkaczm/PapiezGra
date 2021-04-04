9d33fe7ce567218de69458613bf0b29d1db04cb7
Michal K <michal.kaczm@outlook.com>
2021-04-04 02:06:44 +0200

    detekcja kolizji (jeszcze bez niczego sensownego)

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ class PhysicalObject(sprite.Sprite):
                 obj_1 = game_objects[i]
                 obj_2 = game_objects[j]
                 if util.collides_with(obj_1, obj_2):
-                    return True
+                    return True """Chyba się zapierdole ni chuja nic nie mogę wymyśleć"""
 
 class Pilka(PhysicalObject):
     def __init__(self,  *args, **kwargs):
